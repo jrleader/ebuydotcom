678696f4db893e1ea9dddccb63eb5a5d04afeb1d
jrleader <lixiaoming1993@gmail.com>
2022-02-03 23:40:34 +0800

    修复在测试API时遇到的“Could not locate a bind configured..."问题

--- tbmall/models/__init__.py
+++ tbmall/models/__init__.py
@@ -3,3 +3,21 @@
 from .product import Product, ProductSchema
 from .shop import Shop, ShopSchema
 from .favourite_product import FavouriteProduct, FavouriteProductSchema
+
+from sqlalchemy.orm import sessionmaker, Session
+
+from sqlalchemy import create_engine
+
+from ..config import BaseConfig
+
+engine = create_engine(BaseConfig.SQLALCHEMY_DATABASE_URI)
+sessionlocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+session = Session()
+
+def get_db_session():
+    global session
+    session = sessionlocal()
+    try:
+        yield session
+    finally:
+        session.close()
