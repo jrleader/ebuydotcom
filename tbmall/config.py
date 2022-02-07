296ceee89f42b532ec05db31d769f7a5a914d1d9
jrleader <lixiaoming1993@gmail.com>
2022-02-07 23:37:02 +0800

    更新商城服务配置，将数据库密码加密

--- tbmall/config.py
+++ tbmall/config.py
@@ -1,13 +1,18 @@
+from utils import Cryptog as cg
+
 class BaseConfig(object):
     # 这里涉及到数据传递，需要设置 SECRET_KEY
     SECRET_KEY = '4bOoOz6GFmF5vVEPd0SvyOOt7m2b16l6'
     # 设置服务监听地址：http://0.0.0.0:5020
     LISTENER = ('0.0.0.0', 5020)
 
-    SQLALCHEMY_DATABASE_URI = 'mysql+mysqldb://root:Pa$$w0rd@localhost:3306/ebmall?charset=utf8' # Need to encode the db password
+    # 在变量被初始化后即被读取，后续对该变量的更改无效
+    SQLALCHEMY_DATABASE_URI = 'mysql+mysqldb://root:{}@localhost:3306/ebmall?charset=utf8'.format(cg.decrypt_encoded_info('eee942b5ad7245dffe7731380e2b57fc')) # Need to encode the db password
+
     SQLALCHEMY_TRACK_MODIFICATIONS = False # 在不需要跟踪对象变化时降低内存消耗. Flask-SQLAlchemy官方建议
 
     PAGINATION_PER_PAGE = 20 # 列表接口默认返回的每页数据条数
+
 
 
 class DevelopmentConfig(BaseConfig):
