0f48661b754b83981117ebe3ec37225f9c9f912f
jrleader <lixiaoming1993@gmail.com>
2022-02-28 11:15:07 +0800

    添加数据库迁移脚本

---
+++ db/user.sql
@@ -0,0 +1,14 @@
+create database `ebuser`
+default character set utf8mb4;
+
+use `ebuser`;
+
+show tables;
+
+select * from alembic_version;
+
+drop table alembic_version;
+
+select * from users;
+
+select * from addresses;
